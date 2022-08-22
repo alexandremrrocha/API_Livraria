46b95fa57595d6532b3939402eae9f5fb5d68632
Alexandre Miranda Rezende Rocha <alexandre.rocha@tecprime.com.br>
2022-08-22 17:18:50 -0300

    FEAT: Criando a requisição POST e colocando algumas validações

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,11 +2,23 @@ import livros from '../models/Livro.js';
 
 class LivroController {
 
-     static listarLivros = (req, res) =>{
+    static listarLivros = (req, res) => {
         livros.find((err, livros) => {
             res.status(200).json(livros)
         })
-     }
+    }
+
+    static cadastrarLivro = (req, res) => {
+        let livro = new livros(req.body);
+
+        livro.save(() => {
+            if(livro.titulo == undefined || livro.autor == undefined || livro.editora == undefined){
+                res.status(500).send({message: `Falha ao cadastrar o livro`})
+            } else{
+                res.status(201).json(livro)
+            }
+        })
+    }
 }
 
 export default LivroController
