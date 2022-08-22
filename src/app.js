3026b7bc3e1284c79411bd5c946dd0fb9a90ec1b
Alexandre Miranda Rezende Rocha <alexandre.rocha@tecprime.com.br>
2022-08-22 11:59:26 -0300

    FEAT: Metodo PUT, DELETE e GET por id

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import express from "express";
+import db from "./config/dbConnect.js"
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () =>{
+    console.log(`Conexão com o banco feita com sucesso!`)
+})
 
 const app = express();
 
@@ -17,13 +23,32 @@ app.get('/livros', (req, res)=>{
     res.status(200).json(livros)
 })
 
+app.get('/livros/:id', (req, res)=>{
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
 app.post('/livros', (req,res)=>{
     livros.push(req.body);
     res.status(201).send('Livro cadastrado com sucesso!')
 })
 
-// app.put('/livros/:id', (req, res)=>{
+app.put('/livros/:id', (req, res)=>{
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros[index]);
+})
 
-// })
+app.delete('/livros/:id', (req, res)=>{
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1);
+    res.json(`Livro ${id} removido com sucesso!`);
+})
+
+
+function buscaLivro(id){
+    return livros.findIndex(livros => livros.id == id)
+}
 
 export default app
