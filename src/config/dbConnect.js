165f241d35e74442c3bb9d2121394422fb3bc88c
Alexandre Miranda Rezende Rocha <alexandre.rocha@tecprime.com.br>
2022-08-22 15:54:38 -0300

    FIX: Deletando arquivos, e consertando o gitignore

--- src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.w86iyyr.mongodb.net/livraria-node");
-
-let db = mongoose.connection;
-
-export default db;
