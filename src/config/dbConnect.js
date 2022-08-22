a5c90a045c13b8647a525540d91949269a6a24eb
Alexandre Miranda Rezende Rocha <alexandre.rocha@tecprime.com.br>
2022-08-22 15:47:24 -0300

    Revert "FIX: Ajuste na conexão com o banco de dados"
    This reverts commit 813fc3a36c17a016a153ca1bc21ac971113ccd63.

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.w86iyyr.mongodb.net/livraria-node");
+mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.w86iyyr.mongodb.net/");
 
 let db = mongoose.connection;
 
