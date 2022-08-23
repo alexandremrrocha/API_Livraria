3d9d2c3e038ad84400f81fb4a47dd62923116580
Alexandre Miranda Rezende Rocha <alexandre.rocha@tecprime.com.br>
2022-08-23 16:43:45 -0300

    FEAT: Adicionando busca de livro por editora

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,7 +4,7 @@ const livroSchema = new mongoose.Schema(
     {
         id: {type: String},
         titulo: {type: String, require: true},
-        autor: {type: String, require: true},
+        autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', require: true},
         editora: {type: String, require: true},
         numeroPaginas: {type: Number}
     }  
