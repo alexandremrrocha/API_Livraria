9632e9d57a4e35eb975c6572b8cf46a14c060481
Alexandre Miranda Rezende Rocha <alexandre.rocha@tecprime.com.br>
2022-08-23 09:33:49 -0300

    STYLE: Limpando o codigo inutilizavel

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import express from "express";
 import db from "./config/dbConnect.js"
-import livros from "./models/Livro.js"
 import routes from "./routes/index.js"
 
-db.on("error", console.log.bind(console, 'Erro de conexão'))
-db.once("open", () =>{
+db.on("error", console.log.bind(console, 'Erro de conexão com o banco'))
+db.once("open", () => {
     console.log(`Conexão com o banco feita com sucesso!`)
 })
 
@@ -13,28 +12,5 @@ const app = express();
 app.use(express.json())
 
 routes(app);
-
-app.get('/livros/:id', (req, res)=>{
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-})
-
-app.put('/livros/:id', (req, res)=>{
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros[index]);
-})
-
-app.delete('/livros/:id', (req, res)=>{
-    let {id} = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.json(`Livro ${id} removido com sucesso!`);
-})
-
-
-function buscaLivro(id){
-    return livros.findIndex(livros => livros.id == id)
-}
 
 export default app
